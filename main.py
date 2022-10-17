cbe21e226dd23d6d01f9d4e46c18fc1696a0af90
Eric Santos <eric88193@gmail.com>
2022-10-17 20:33:57 -0300

    .

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
-import scrapy  
+import scrapy
 
 class AluraBot(scrapy.Spider):
-    name = "Bot Alura"
-    start_urls =["https://www.alura.com.br/cursos-online-programacao"]
-    
+    name = "Alura Bot"
+    start_urls = ["https://www.alura.com.br/cursos-online-programacao"]
+
     def parse(self, response):
         SELETOR = ".subcategoria__item"
         cursos = []
-
         for categoria in response.css(SELETOR):
             curso = {}
 
             NOME_SELETOR = ".card-curso__nome ::text"
             LINK_SELECTOR = ".card-curso ::attr(href)"
 
-            curso ['nome'] = categoria.css (NOME_SELETOR)
-            curso ['link'] = "https://www.alura.com.br" + categoria.css(LINK_SELECTOR)
-
+            curso['nome'] = categoria.css(NOME_SELETOR).extract_first()
+            curso['link'] = "https://www.alura.com.br" + categoria.css(LINK_SELECTOR).extract_first()
             print(curso)
-            cursos.append(curso)
 
-        print("Total de Cursos:", len(cursos))
+            cursos.append(curso)
         
+        print("Total de Cursos: ", len(cursos))
         
         """ execução = scrapy runspider + nome do arquivo .py"""
